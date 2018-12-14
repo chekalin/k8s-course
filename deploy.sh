751eeb3eedf770070ff597096a375fcb36092e28
schekalin <stanislav.chekalin@sigfig.com>
2018-12-14 11:57:18 +0000

    adding Travis configuration for continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+docker build -t chekalin/multi-client:latest -t chekalin/multi-client:${SHA} -f ./client/Dockerfile ./client
+docker build -t chekalin/multi-server:latest -t chekalin/multi-server:${SHA} -f ./server/Dockerfile ./server
+docker build -t chekalin/multi-worker:latest -t chekalin/multi-worker:${SHA} -f ./worker/Dockerfile ./worker
+docker push chekalin/multi-client:latest
+docker push chekalin/multi-server:latest
+docker push chekalin/multi-worker:latest
+docker push chekalin/multi-client:${SHA}
+docker push chekalin/multi-server:${SHA}
+docker push chekalin/multi-worker:${SHA}
+kubectl apply -f k8s
+kubectl set image deployemnts/client-deployment client=chekalin/multi-client:${SHA}
+kubectl set image deployemnts/server-deployment server=chekalin/multi-server:${SHA}
+kubectl set image deployemnts/worker-deployment worker=chekalin/multi-worker:${SHA}
